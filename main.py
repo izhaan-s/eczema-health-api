ec94630822d9e38d51e789c40e20a6c1fc2a8230
Izhaan Salam <izhaan.salam@student.manchester.ac.uk>
2025-05-18 18:36:22 +0100

    Updated routing

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-from app.api.insights import router as insights_router
+from app.api.flare import router as flare_router
+from app.api.symptom_insight import router as symptom_insight_router    
+
 api = FastAPI()
 
 @api.get("/")
 def read_root():
     return {"message": "Hello World"}
 
-api.include_router(insights_router)
+api.include_router(flare_router)
+api.include_router(symptom_insight_router)
